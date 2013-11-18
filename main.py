22024ac8e12d211f58619f2baae8103e4a06c5b6
tonghs <tonghuashuai@126.com>
2013-11-18 21:25:53 +0800

    完成新建功能

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 #coding=utf-8
 import json
 import web
-import os
+import sqlite3
+from configs import db
+
 __author__ = 'Administrator'
 
 urls = (
     '/push', 'push',
-    '/', 'index'
+    '/', 'index',
+    '/install', 'install',
+    '/new_job', 'new_job'
 )
 
 app = web.application(urls, globals())
@@ -18,7 +22,13 @@ if __name__ == '__main__':
 
 class index:
     def GET(self):
-        return render.index()
+        conn = sqlite3.connect(db.DB)
+        cur = conn.cursor()
+        cur.execute('select id, name, addr from tb_job;')
+        jobs = []
+        for job in cur:
+            jobs.append(job)
+        return render.index(jobs)
 
 
 class push:
@@ -30,3 +40,32 @@ class push:
         print url
 
         return render.push(data)
+
+
+class install:
+    def GET(self):
+        conn = sqlite3.connect(db.DB)
+        conn.execute(db.CREATE_JOB_SQL)
+        conn.execute(db.CREATE_QUEUE_SQL)
+        conn.execute(db.CREATE_LOG_SQL)
+        conn.close()
+        return web.seeother('/')
+
+
+class new_job:
+    def GET(self):
+        return render.new_job()
+
+    def POST(self):
+        data = web.input()
+        addr = data.addr
+        cmd = data.cmd
+        name = addr[addr.rfind('/') + 1: -4]
+        dir = data.dir
+        params = [(name, dir, cmd, addr)]
+        conn = sqlite3.connect(db.DB)
+        conn.executemany(db.INSERT_TO_JOB, params)
+        conn.commit()
+        conn.close()
+
+        return web.seeother('/')
