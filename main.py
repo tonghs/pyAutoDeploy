652f7ee2d89d6804f0924be796ff197b31792957
tonghuashuai <tonghuashuai@126.com>
2013-11-18 17:08:24 +0800

    新建文件

---
+++ main.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+import web
+import os
+__author__ = 'Administrator'
+
+urls = (
+    '/push', 'push',
+    '/', 'index'
+)
+
+app = web.application(urls, globals())
+render = web.template.render('templates')
+
+if __name__ == '__main__':
+    app.run()
+
+
+class index:
+    def GET(self):
+        return render.index()
+
+
+class push:
+    def POST(self):
+        data = web.input()
+        #os.popen('nginx -s reload')
+        for d in data:
+            print '%s:%s' % (d, data[d])
+        return render.push(data)
