f02a744bd36b0217507d8c18c5e7be704c7f1f27
tonghuashuai <tonghuashuai@126.com>
2013-11-20 10:55:18 +0800

    样式调整

--- configs/db.py
+++ configs/db.py
@@ -4,9 +4,9 @@ __author__ = '华帅'
 DB = 'db'
 
 CREATE_JOB_SQL = 'create table tb_job (id integer primary key autoincrement,' \
-                 ' name varchar(100), dir varchar(100), cmd text, addr varchar(500));'
+                 ' name varchar(100), dir varchar(100), addr varchar(500), exe_time datetime);'
 CREATE_LOG_SQL = 'create table tb_log (id integer primary key autoincrement, ' \
                  'job_id integer, exe_time varchar(19), content text);'
 CREATE_QUEUE_SQL = 'create table tb_queue (id integer primary key autoincrement, job_id integer, att_cmd text);'
 
-INSERT_TO_JOB = 'insert into tb_job (name, dir, cmd, addr) values (?, ?, ?, ?);'
+INSERT_TO_JOB = 'insert into tb_job (name, dir, addr) values (?, ?, ?);'
--- main.py
+++ main.py
@@ -10,22 +10,27 @@ __author__ = 'Administrator'
 urls = (
     '/push', 'push',
     '/', 'index',
+    '/favicon.ico', 'faviconICO',
     '/install', 'install',
     '/new_job', 'new_job'
 )
 
 app = web.application(urls, globals())
-render = web.template.render('templates')
+render = web.template.render('templates/', base='base')
 
 if __name__ == '__main__':
     app.run()
 
 
+class faviconICO(object):
+   def GET(self):
+       return web.seeother('/static/images/favicon.ico')
+
 class index:
     def GET(self):
         conn = sqlite3.connect(db.DB)
         cur = conn.cursor()
-        cur.execute('select id, name, addr from tb_job;')
+        cur.execute('select name, addr, exe_time, id from tb_job;')
         jobs = []
         for job in cur:
             jobs.append(job)
@@ -37,14 +42,14 @@ class push:
         data = web.input()
         data = json.loads(data.payload)
         url = data['repository']['url']
-        sql = 'select dir, name from tb_job where addr = "%s.git"' % url
+        sql = 'select dir, name from tb_job where addr = "%s"' % url
         conn = sqlite3.connect(db.DB)
         cur = conn.cursor()
         cur.execute(sql)
         for job in cur:
             os.chdir('%s/%s' % (job[0], job[1]))
             os.popen('git pull')
-            os.popen('nginx -s reload')
+            os.popen('./cmd.sh')
         print url
 
         return render.push(data)
@@ -67,10 +72,9 @@ class new_job:
     def POST(self):
         data = web.input()
         addr = data.addr
-        cmd = data.cmd
-        name = addr[addr.rfind('/') + 1: -4]
+        name = addr[addr.rfind('/') + 1:]
         dir = data.dir
-        params = [(name, dir, cmd, addr)]
+        params = [(name, dir, addr)]
         conn = sqlite3.connect(db.DB)
         conn.executemany(db.INSERT_TO_JOB, params)
         conn.commit()
